9879a386009bf34635082fc312cbdc3f6a4b4609
Musharraf188 <mr.musharraf188@gmail.com>
2023-07-04 21:29:02 +0530

    ExpenseItem/Contine

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import './ExpenseItem.css';
+
+const ExpenseItem = ()=>{
+return <div className='expense-item'>
+    <div>Date</div>
+    <div>Title</div>
+    <div>Amount</div>
+</div>
+}
+
+export default ExpenseItem;
