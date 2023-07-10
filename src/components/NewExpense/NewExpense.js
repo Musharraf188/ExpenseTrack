0ac161d6b1df5f7ca3f696af7a8bac351567c371
Musharraf188 <mr.musharraf188@gmail.com>
2023-07-10 16:34:08 +0530

    Chart/Created

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,16 +3,22 @@ import ExpenseForm from '../Expenses/ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense =(props)=>{
-    const [isValid , setIsValid] = useState('');
+    const [isValid , setIsValid] = useState(false);
     const userInput =(userInput)=>{
         const expenseData={
         ...userInput
         }
 props.OnAdd(expenseData)
     }
+    const onClickHandler=()=>{
+        setIsValid(true);
+    }
+    const cancelHandler =()=>{
+        setIsValid(false);
+    }
     return <div className='new-expense'>
-       {!isValid && <button type='button'>Add New Expenses</button>}
-       {isValid && <ExpenseForm onSaveData={userInput}/>}
+       {!isValid && <button type='button' onClick={onClickHandler}>Add New Expenses</button>}
+       {isValid && <ExpenseForm onCancel={cancelHandler} onSaveData={userInput}/>}
     </div>
 }
 
